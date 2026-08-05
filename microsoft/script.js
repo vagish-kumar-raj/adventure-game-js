66af1f5d9e9f3d75d740906c818721ad736fc0e3
vagish-kumar-raj <230776036+vagish-kumar-raj@users.noreply.github.com>
2026-08-05 15:01:55 +0530

    Add initial JavaScript script with greeting messages

---
+++ microsoft/script.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+/* 
+This is a multi-line comment.
+! Anatomy of: console.log("Hello, World!");
+* console           → A toolbox for developers.
+* .                 → Opens the toolbox.
+* log               → The tool that prints a message.
+* ()                → Where you put what you want to print.
+* "Hello, World!"   → The message to display.
+* ;                 → Ends the instruction.
+*/
+//this is a single-line comment. Comments are ignored by the JavaScript engine and are used for adding notes or explanations in the code.
