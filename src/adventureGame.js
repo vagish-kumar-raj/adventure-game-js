1f9f228d54e52bcfd1b7c4c569b0bb57ec5589bc
vagish-kumar-raj <230776036+vagish-kumar-raj@users.noreply.github.com>
2026-08-06 22:54:16 +0530

    Refactor welcome message and improve game introduction

--- src/adventureGame.js
+++ src/adventureGame.js
@@ -5,11 +5,13 @@ const readline = require("readline-sync");
 This game will be a text-based adventure game where the player will be able to make choices that affect the outcome of the game.
 The player will be able to choose their own path and the story will change based on their decisions.
 */
-
-// * Welcome Message
-console.log("Welcome to the Adventure Game! \nPrepare yourself for an epic journey!");
+console.log("=================================");
+console.log("||     The Dragon's Quest      ||");
+console.log("=================================");
+console.log("Defeat the dragon in the mountains!");
+console.log("Prepare yourself for an epic journey!");
 // Get player name using readline-sync
-let playerName = readline.question("What your player's name? : ");
+let playerName = readline.question("What's your name? : ");
 // Create variables for player stats
 let playerHealth = 100;
 let playerGold = 20;
